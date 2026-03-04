6eb2e70dc7f1ae1b6f809e4e0fb5f03d8ecf1ce1
suhruth <suhruthkiyer@gmail.com>
2026-03-04 17:43:42 -0500

    suhruth

--- game.js
+++ game.js
@@ -36,7 +36,7 @@
 
   // Load background images
   const backgroundImg = new Image();
-  backgroundImg.src = 'comic_forest.png';
+  backgroundImg.src = 'comic_forest.jpeg';
 
   let width = 800;
   let height = 600;
